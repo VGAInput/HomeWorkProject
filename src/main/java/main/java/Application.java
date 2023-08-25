266fd62142d1e1780d87ac0266aedf3bf15a9472
Ivan Paskhin <ivanpaskhin@yandex.ru>
2023-08-25 20:02:36 +0300

    Update Application.java

--- src/main/java/main/java/Application.java
+++ src/main/java/main/java/Application.java
@@ -7,7 +7,7 @@ public class Application {
     public static void main(String[] args) throws SQLException {
 
         final String user = "postgres";
-        final String password = "0451";
+        final String password = "0000";
         final String url = "jdbc:postgresql://localhost:5432/skypro";
 
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
